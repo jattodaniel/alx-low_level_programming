839ccc6cbab0c8b28034f90c612e313b7be64b60
jattodaniel <iamonimisi@gmail.com>
2022-09-09 15:27:08 -0700

    try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #!/bin/bash
-#include <stdio.h>
+'''#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -10,3 +10,14 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+'#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+        puts("Programming is like building a multilingual puzzle);
+        return (0);
+}'''
