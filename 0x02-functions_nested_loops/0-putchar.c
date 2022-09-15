b1416f030dcea90347a65b963c44deb40ed8ee8e
jattodaniel <iamonimisi@gmail.com>
2022-09-14 20:15:01 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
+
 /**
- * main -Entry point
- *
- * Return: Always success
+ * main - print putchar
+ * Return: always 0
  */
+
 int main(void)
 {
-printf("_putchar\n");
+	 _putchar('_');
+	 _putchar('p');
+	 _putchar('u');
+	 _putchar('t');
+	 _putchar('c');
+	 _putchar('h');
+	 _putchar('a');
+	 _putchar('r');
+	 _putchar('\n');
 
-return (0);
+	 return(0);
+
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 
+int _âˆputchar(char);
 void print_alphabet(void);
-print_alphabet_x10(void);
-int islower(int c);
-int _putchar(char c);
+void print_alphabet_x10(void);
+int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int n);
+int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
+void num(int r, int n);
 
-#endif
