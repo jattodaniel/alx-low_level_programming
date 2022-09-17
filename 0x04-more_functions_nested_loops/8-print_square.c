6e15733843f4d6937fdc69162015332e25a0dbea
jattodaniel <iamonimisi@gmail.com>
2022-09-16 19:03:41 -0700

    first 10

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+Å#include <stdio.h>
+#include "main.h"
+
+/**
+*print_square - prints squares
+*@size: parameterâ…
+*Return: returns nothing
+*/
+
+void print_square(int size)
+{
+	int inc1, inc2;
+
+	if (size > 0)
+	{
+		for (inc1 = 0; inc1 < size; inc1++)
+		{
+			putchar('#');
+			putchar('\n');
+		}
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
