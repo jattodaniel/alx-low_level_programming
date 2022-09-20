76724d8f584df5441ba392fff74f94b1a5ec730c
jattodaniel <iamonimisi@gmail.com>
2022-09-19 21:59:01 -0700

    is going

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - it updates the value pointed to 98
+ * @n: points to n
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - it swaps the value of two integers
+ *
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int aux;
+
+	aux = *a;
+	*a = *b;
+}
