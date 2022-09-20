7258d27108228fd6245ef754fb512c0e7407e09c
jattodaniel <iamonimisi@gmail.com>
2022-09-19 22:31:57 -0700

    yes!

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - it prints a string in reverse
+ * @s: string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+}
