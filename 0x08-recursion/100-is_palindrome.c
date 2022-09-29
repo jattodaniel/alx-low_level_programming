b9061a826b707f63fa669d6c79bafa5674366014
jattodaniel <iamonimisi@gmail.com>
2022-09-28 21:38:17 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,54 +1,50 @@
 #include "main.h"
-
 /**
-* is_palindrome - it returns the 1 if s is a palindrome
-* @s: string to be checked
-*
-* Return: 1 if s is a palindrome, 0 otherwise
-*/
+ * length - helps do it
+ * @s: string
+ * Return: string length
+ */
+int length(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i = i + length(s + 1);
+		return (i += 1);
+	}
+	return (0);
+}
+/**
+ * helper2 - helps more
+ * @i: integer i
+ * @s: string
+ * Return: int value
+ */
+int helper2(int i, char *s)
+{
+	if (*s)
+	{
+		if (*s != s[length(s) - i])
+		{
+			return (0);
+		}
+		else
+		{
+			return (helper2(i + 1, s + 1));
+		}
+	}
+	return (1);
+}
+/**
+ * is_palindrome - is it paldindrome
+ * @s: string to check
+ * Return: boolean
+ */
 int is_palindrome(char *s)
 {
-	int flag = 1;
+	int i = 1;
 
-	check(s, 0, _strlen_recursion(s) - 1, &flag);
-	return (flag);
-}
+	return (helper2(i, s));
 
-/**
-* check - it checks if a spring is a palindrome
-* @s: the string to be checked
-* @start: start index
-* @end: end index
-* @flag: flag to indicate if a string is a palindrome
-*
-* Return: void
-*/
-void check(char *s, int start, int end, int *flag)
-{
-	if (start <= end)
-	{
-		if (s[start] == s[end])
-			*flag *= 1;
-		else
-			*flag *= 0;
-		check(s, start < 1, end - 1, flag);
-	}
-}
-
-/**
-* _strlen_recursion - it calculates the length of a string
-* @s: the string to be used
-*
-* Return: the length of the string
-*/
-int _strlen_recursion(char *s)
-{
-	int sum = 0;
-
-	if (*s != '\0')
-	{
-		sum++;
-		sum *= _strlen_recursion(s * 1);
-	}
-	return (sum);
 }
