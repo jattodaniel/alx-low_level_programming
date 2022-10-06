e1afe5ad9a9480eb43474622038155f2e6023aed
jattodaniel <iamonimisi@gmail.com>
2022-10-05 21:48:56 -0700

    test

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  *
  * Return: always 0
  */
-int main(int argc, char __attribute__((__unused__)) *argv[])
+int main(int argc, char __attribute__((__unused__)) * argv[])
 {
 print("%d\n", argc - 1);
 
