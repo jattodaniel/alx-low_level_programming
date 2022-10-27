8926bda3fb9fcc7a78fd986c0d70a4e23bb7d5c4
jattodaniel <iamonimisi@gmail.com>
2022-10-26 18:45:54 -0700

    correct 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,9 +1,9 @@
-#include <stdio>
-/*
- * stint_len - count thennumber of nodes in a linked list
+#include "lists.h"
+/**
+ * listint_len - counts the number of nodes in a linked list
  * @h: head of the list
  *
- * Return: the number of eleemnts
+ * Return: the number of elements
  */
 size_t listint_len(const listint_t *h)
 {
